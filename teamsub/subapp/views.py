516c756eee9f1364ca48bbd021f0b89b418f5799
noufalkakdlr <noufalkakdlr@gmail.com>
2024-11-29 16:17:46 +0530

    list

--- teamsub/subapp/views.py
+++ teamsub/subapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from . import models
 
 def index(request):
     return render(request,'index.html')
+
+def list(request):
+    return render(request,'list.html')
