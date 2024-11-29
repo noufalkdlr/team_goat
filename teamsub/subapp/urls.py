7aac1d09d7431f1fc09cf0658035f452dd2c470e
noufalkakdlr <noufalkakdlr@gmail.com>
2024-11-29 22:04:52 +0530

    delete button

--- teamsub/subapp/urls.py
+++ teamsub/subapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns=[
     path('',views.index,name='index'),
     path('list/',views.list,name='list'),
+    path('deletemovie/<int:id>',views.movie_delete,name = 'deletemovie')
+    
 ]
--- teamsub/subapp/views.py
+++ teamsub/subapp/views.py
@@ -3,9 +3,25 @@ from . import models
 from . import forms
 
 def index(request):
+
+    if request.POST:
+        frm = forms.MovieForm(request.POST)
+        if frm.is_valid():
+            frm.save()
+    else:
+        frm = forms.MovieForm()
     
-    return render(request, 'index.html',)
+    Movie_frm ={
+        'frm':frm
+    }
+    
+    return render(request, 'index.html',Movie_frm)
 
 def list(request):
     movie_list =models.MovieInfo.objects.all()
     return render(request,'list.html',{'movie':movie_list})
+
+def movie_delete(requset, id):
+    dele = models.MovieInfo.objects.get(id=id)
+    dele.delete()
+    return redirect('/list')
